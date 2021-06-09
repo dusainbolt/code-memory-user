c7e4e562b0588a1bab28a124416d80b7c7c77e2b
kai <dulh181199@gmail.com>
2021-06-09 22:07:53 +0700

    make

--- src/components/Header/menuHeader.tsx
+++ src/components/Header/menuHeader.tsx
@@ -1,35 +1,23 @@
 import useTranslation from '@Components/LanguageProvider/useTranslation';
 import { LIST_MENU } from '@Config/contains';
 import { useRouter } from 'next/router';
-import { Menu } from 'antd';
 import Link from 'next/link';
-import { MenuOutlined } from '@ant-design/icons';
 
 export interface _MenuHeader {}
 
 const MenuHeader: React.FC<_MenuHeader> = ({}) => {
-    const router = useRouter();
-    const { t } = useTranslation();
+  const router = useRouter();
+  const { t } = useTranslation();
 
-    // const classActive = href => {
-    //     return clsx('menu--link', {
-    //         active: router.pathname.indexOf(href) !== -1,
-    //     });
-    // };
-
-    return (
-        <Menu
-            selectedKeys={['mail']}
-            overflowedIndicator={<MenuOutlined className="header--menu-icon-collapse" />}
-            mode="horizontal"
-            className="app-menu header--menu">
-            {LIST_MENU.map((item, index) => (
-                <Menu.Item key={index}>
-                    <Link href={item.href}>{t(`menu.${item.name}`)}</Link>
-                </Menu.Item>
-            ))}
-        </Menu>
-    );
+  return (
+    <nav className="header--menu">
+      {LIST_MENU.map((item, index) => (
+        <Link key={index} href={item.href}>
+          {t(`menu.${item.name}`)}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default MenuHeader;
--- src/config/contains.ts
+++ src/config/contains.ts
@@ -1,39 +1,39 @@
 export const FONT_SIZE = 16;
 
 export const STATUS = {
-    NORMAL: "",
-    ACTIVE: "active",
-}
+  NORMAL: '',
+  ACTIVE: 'active',
+};
 
 export const LIST_MENU = [
-    { name: 'txt_home', href: '/' },
-    { name: 'txt_blog', href: '/blog' },
-    { name: 'txt_about', href: '/about' },
-    { name: 'txt_contact', href: '/contact' },
-    { name: 'txt_services', href: '/service' },
+  { name: 'txt_home', href: '/' },
+  { name: 'txt_blog', href: '/blog' },
+  { name: 'txt_about', href: '/about' },
+  { name: 'txt_contact', href: '/contact' },
+  { name: 'txt_services', href: '/service' },
 ];
 
 export const OPTION_LANG = [
-    {
-        src: '/images/vn_flag.svg',
-        value: 'vn',
-    },
-    {
-        src: '/images/en_flag.svg',
-        value: 'en'
-    }
-]
+  {
+    src: '/images/vn_flag.svg',
+    value: 'vn',
+  },
+  {
+    src: '/images/en_flag.svg',
+    value: 'en',
+  },
+];
 
 export const OPTION_THEME = {
-    light: 'light',
-    dark: 'dark'
-}
+  light: 'light',
+  dark: 'dark',
+};
 
 export const VIEW_SCREEN_WIDTH = {
-    xs: 576,
-    sm: 768,
-    md: 992,
-    lg: 1200,
-    xl: 1366,
-    xxl: 1920,
+  xs: 576,
+  sm: 768,
+  md: 992,
+  lg: 1200,
+  xl: 1366,
+  xxl: 1920,
 };
