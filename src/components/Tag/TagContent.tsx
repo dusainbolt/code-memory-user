408114dda5b5bba5c8d16b0b94c22c80356b860e
Du Le <du.le@ekoios.vn>
2021-09-21 11:31:12 +0700

    demo-2

--- src/components/Tag/TagContent.tsx
+++ src/components/Tag/TagContent.tsx
@@ -3,22 +3,17 @@ import { FC } from 'react';
 import BoxWellComePage from './BoxWellComePage';
 import { Col, Row } from 'antd';
 import ListTagContent from './ListTagContent';
-import { FireFilled, RiseOutlined } from '@ant-design/icons';
+import { RiseOutlined } from '@ant-design/icons';
 import { useAppSelector } from '@Redux/store';
 import { getTagSlice } from '@Redux/slices/tagSlice';
 
 const TagContent: FC<any> = () => {
   const { dataNew } = useAppSelector(getTagSlice);
+
   return (
     <Box className="tag-content mt-26">
       <BoxWellComePage title={'tag.title_welcome'} description={'tag.des_welcome'} />
       <Row gutter={[16, 16]} className="tag-content__wrap">
-        <Col xs={24} xl={12}>
-          <ListTagContent data={[]} title="tag.top_month" description="tag.des_top_month" icon={<FireFilled />} />
-        </Col>
-        <Col xs={24} xl={12}>
-          <ListTagContent data={[]} title="tag.top_to_now" description="tag.des_top_to_now" icon={<FireFilled />} />
-        </Col>
         <Col xs={24}>
           <ListTagContent data={dataNew} className="new-tag-content" title="tag.new" description="tag.des_new" icon={<RiseOutlined />} />
         </Col>
--- src/pages/tag/[slug].tsx
+++ src/pages/tag/[slug].tsx
@@ -6,27 +6,27 @@ import { FindTagBySlugInput, Tag, TagStatus } from '@Models/TagModel';
 import { GetStaticPaths } from 'next';
 import { getEntireTags, getTagBySlugRequest } from '@GraphQL/tagRequest';
 import { getTagSlice, getTagDetailSuccess } from '@Redux/slices/tagSlice';
-import { getSeoHomeSuccess } from '@Redux/slices/seoHomeSlice';
-import { getSeoHomeRequest } from '@GraphQL/seoHomeRequest';
 import { useRouter } from 'next/dist/client/router';
-import { Descriptions } from 'antd';
+import { Descriptions, Skeleton } from 'antd';
 import AntImage from '@Common/Image';
 
 const TagDetail: React.FC<any> = () => {
   const data = useAppSelector(getTagSlice).tagDetail;
   const router = useRouter();
   if (router.isFallback) {
-    return <div>Loading....................NEXT....................JS..</div>;
+    return <Skeleton avatar paragraph={{ rows: 10 }} />;
   }
   return (
     <LayoutCommon header={false} footer={false} scrollHeader>
-      <Descriptions title="Tag Info" layout="vertical">
-        <Descriptions.Item label="Title">{data.title}</Descriptions.Item>
-        <Descriptions.Item label="Description">{data.description}</Descriptions.Item>
-        <Descriptions.Item label="Thumbnail">
-          <AntImage width={100} height={100} src={data.thumbnail} />
-        </Descriptions.Item>
-      </Descriptions>
+      <div style={{ padding: 20 }}>
+        <Descriptions title="Tag Info" layout="vertical">
+          <Descriptions.Item label="Title">{data.title}</Descriptions.Item>
+          <Descriptions.Item label="Description">{data.description}</Descriptions.Item>
+          <Descriptions.Item label="Thumbnail">
+            <AntImage width={100} height={100} src={data.thumbnail} />
+          </Descriptions.Item>
+        </Descriptions>
+      </div>
     </LayoutCommon>
   );
 };
@@ -35,18 +35,21 @@ export default TagDetail;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Get List Slug Tag
-  const listSlugTags: Tag[] = await getEntireTags({ status: [TagStatus.ACTIVE] });
+  const listSlugTags: Tag[] = await getEntireTags({ limit: 5, status: [TagStatus.ACTIVE] });
 
   // Create list Path
   const paths: ParamsPathSlug[] = listSlugTags.map(item => ({ params: { slug: item.slug }, locale: 'vi' }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 export const getStaticProps = wrapper.getStaticProps(async ({ locale, params, store }: SSRContext) => {
   try {
     // Call api fetch data
     const tagDetail = await getTagBySlugRequest(params as FindTagBySlugInput);
+
+    // Create delay
+    await new Promise(res => setTimeout(res, 5000));
 
     // Save data to store Redux
     store.dispatch(getTagDetailSuccess(tagDetail));
